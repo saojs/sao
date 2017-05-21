b8a51d0c8f51f4faf3a8361b9728dbb4aeb71fb1
EGOIST <0x142857@gmail.com>
2017-05-22 01:24:41 +0800

    use prettier

--- test/index.test.js
+++ test/index.test.js
@@ -2,23 +2,30 @@ const path = require('path')
 const sao = require('..')
 
 test('simple template', () => {
-  return sao.mockPrompt({
-    fromPath: path.join(__dirname, 'fixture/simple'),
-    targetPath: path.join(__dirname, 'output/simple')
-  }, {
-    test: false
-  }).then(({ fileList, files }) => {
-    expect(fileList).toEqual(['.gitignore', 'bar/bar.js', 'foo.js'])
-    expect(files['foo.js'].contents.toString()).toMatch('no')
-  })
+  return sao
+    .mockPrompt(
+      {
+        fromPath: path.join(__dirname, 'fixture/simple'),
+        targetPath: path.join(__dirname, 'output/simple')
+      },
+      {
+        test: false
+      }
+    )
+    .then(({ fileList, files }) => {
+      expect(fileList).toEqual(['.gitignore', 'bar/bar.js', 'foo.js'])
+      expect(files['foo.js'].contents.toString()).toMatch('no')
+    })
 })
 
 // Not a template (no sao.js), simply copy the whole root foler
 test('non-template', () => {
-  return sao.mockPrompt({
-    fromPath: path.join(__dirname, 'fixture/non-template'),
-    targetPath: path.join(__dirname, 'output/non-template')
-  }).then(({ fileList }) => {
-    expect(fileList).toEqual(['a.js', 'b.js'])
-  })
+  return sao
+    .mockPrompt({
+      fromPath: path.join(__dirname, 'fixture/non-template'),
+      targetPath: path.join(__dirname, 'output/non-template')
+    })
+    .then(({ fileList }) => {
+      expect(fileList).toEqual(['a.js', 'b.js'])
+    })
 })
