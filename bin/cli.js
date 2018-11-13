4fc26cce053d70bf1ca72421c87d68b47a9fbd38
EGOIST <0x142857@gmail.com>
2018-11-13 12:05:34 +0800

    feat(cli): allow to set an alias name for a generator, added set-alias get-alias commands.

--- bin/cli.js
+++ bin/cli.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 const cac = require('cac').default
+const SAOError = require('../lib/SAOError')
 
 const cli = cac()
 
@@ -40,6 +41,28 @@ cli
     desc: 'Use the default options',
     alias: 'y'
   })
+
+cli.command('set-alias', 'Set an alias for a generator path', input => {
+  const store = require('../lib/store')
+  const { escapeDots } = require('../lib/utils/common')
+  const logger = require('../lib/logger')
+
+  const name = input[0]
+  const value = input[1]
+  if (!name || !value) {
+    throw new SAOError(`Invalid arguments: sao set-alias <alias> <generator>`)
+  }
+
+  store.set(`alias.${escapeDots(name)}`, value)
+  logger.success(`Added alias '${name}'`)
+})
+
+cli.command('get-alias', 'Get the generator for an alias', input => {
+  const store = require('../lib/store')
+  const { escapeDots } = require('../lib/utils/common')
+
+  console.log(store.get(`alias.${escapeDots(input[0])}`))
+})
 
 cli.on('error', error => {
   return require('..').handleError(error)
