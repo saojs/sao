3ad34b004b487a4905a13086809cff29a4537a3a
EGOIST <0x142857@gmail.com>
2018-11-25 23:49:16 +0800

    feat(cli): upgrade to CAC 6

--- bin/cli.js
+++ bin/cli.js
@@ -1,80 +1,61 @@
 #!/usr/bin/env node
-const cac = require('cac').default
-const SAOError = require('../lib/SAOError')
+const cac = require('cac')
+const pkg = require('../package')
 
 const cli = cac()
 
 cli
-  .command(
-    '*',
-    {
-      desc: 'Run a generator',
-      alias: 'run'
-    },
-    (input, flags) => {
-      const options = Object.assign(
-        {
-          generator: input[0],
-          outDir: input[1] || '.',
-          updateCheck: true
-        },
-        flags
-      )
+  .command('<generator> [outDir]', 'Run a generator')
+  .action((generator, outDir, flags) => {
+    const options = Object.assign(
+      {
+        generator,
+        outDir: outDir || '.',
+        updateCheck: true
+      },
+      flags
+    )
 
-      if (!options.generator) {
-        return cli.showHelp()
-      }
-
-      return require('../')(options).run()
+    if (!options.generator) {
+      return cli.showHelp()
     }
+
+    return require('../')(options)
+      .run()
+      .catch(err => {
+        require('..').handleError(err)
+      })
+  })
+  .option(
+    '--npm-client <client>',
+    `Use a specific npm client ('yarn' or 'npm')`
   )
-  .option('npm-client', {
-    desc: `Use a specific npm client ('yarn' or 'npm')`,
-    type: 'string'
-  })
-  .option('update', {
-    desc: 'Update cached generator',
-    type: 'boolean',
-    alias: 'u'
-  })
-  .option('clone', {
-    desc: 'Clone repository instead of archive download',
-    type: 'boolean',
-    alias: 'c'
-  })
-  .option('yes', {
-    desc: 'Use the default options',
-    alias: 'y'
-  })
-  .option('registry', {
-    desc: 'Use a custom registry for npm and yarn',
-    type: 'string'
+  .option('-u, --update', 'Update cached generator')
+  .option('-c, --clone', 'Clone repository instead of archive download')
+  .option('-y, --yes', 'Use the default options')
+  .option('--registry <registry>', 'Use a custom registry for npm and yarn')
+
+cli
+  .command('set-alias <name> <value>', 'Set an alias for a generator path')
+  .action((name, value) => {
+    const store = require('../lib/store')
+    const { escapeDots } = require('../lib/utils/common')
+    const logger = require('../lib/logger')
+
+    store.set(`alias.${escapeDots(name)}`, value)
+    logger.success(`Added alias '${name}'`)
   })
 
-cli.command('set-alias', 'Set an alias for a generator path', input => {
-  const store = require('../lib/store')
-  const { escapeDots } = require('../lib/utils/common')
-  const logger = require('../lib/logger')
+cli
+  .command('get-alias <name>', 'Get the generator for an alias')
+  .action(name => {
+    const store = require('../lib/store')
+    const { escapeDots } = require('../lib/utils/common')
 
-  const name = input[0]
-  const value = input[1]
-  if (!name || !value) {
-    throw new SAOError(`Invalid arguments: sao set-alias <alias> <generator>`)
-  }
+    console.log(store.get(`alias.${escapeDots(name)}`))
+  })
 
-  store.set(`alias.${escapeDots(name)}`, value)
-  logger.success(`Added alias '${name}'`)
-})
-
-cli.command('get-alias', 'Get the generator for an alias', input => {
-  const store = require('../lib/store')
-  const { escapeDots } = require('../lib/utils/common')
-
-  console.log(store.get(`alias.${escapeDots(input[0])}`))
-})
-
-cli.on('error', error => {
-  return require('..').handleError(error)
-})
+cli.version(pkg.version)
+cli.help()
 
 cli.parse()
